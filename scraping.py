ed6c13d598b8c437c8337c371aec4bde4fd8aca7
TORITZA <terratreehive@gmail.com>
2025-12-19 12:22:26 -0500

    completed csv_scraper func; opted for pandas instead of csv

--- scraping.py
+++ scraping.py
@@ -1,16 +1,20 @@
 # scrap CSV file containing words, their Parts of Speech, and various definitions, constructed from 
 # a personal discord channel 
-import csv
-
-def csv_scraper(filename: str):
-    with open(filename) as f:
-        content = csv.reader(f)
-        next(content)
-        for _, _, _, text, _, _ in content:
-            pass
+import pandas as pd
 
 
-
-
+def csv_scraper(filename: str) -> list:
+    strip = []
+    data = pd.read_csv(filename, skipfooter=1, skip_blank_lines=True, engine='python')
+    for i, row in data.iterrows():
+        if row['Content'][0:2] == ':X':
+            strip.append(row['Content'][14:].strip())
+        elif row['Content'][0:2] == ':h':
+            strip.append(row['Content'][17:].strip())
+        else:
+            strip.append(row['Content'].strip())
+    return strip
+                 
+                 
 if __name__ == '__main__':
-    csv_scraper() 
+    print(csv_scraper('new-wordSample.csv'))
