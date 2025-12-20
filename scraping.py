7bc886fefced5d7849ac909ae6435ec33e4b08cb
TORITZA <terratreehive@gmail.com>
2025-12-20 07:56:41 -0500

    dict_compiler created; text format precludes intended function

--- scraping.py
+++ scraping.py
@@ -1,6 +1,7 @@
 # scrap CSV file containing words, their Parts of Speech, and various definitions, constructed from 
 # a personal discord channel 
 import pandas as pd
+from markdown_text_clean import clean_text
 
 
 def csv_scraper(filename: str) -> list:
@@ -23,6 +24,16 @@ def csv_scraper(filename: str) -> list:
             strip.append(row['Content'].strip())
     return strip
                  
+def dict_compiler(words: list) -> dict:
+    compiled = {}
+    for item in words:
+        if '-' in item.split():
+            item.split().pop()
+        compiled[clean_text(item.split()[0])] = (clean_text(item.split()[1]), clean_text(' '.join((item.split()[2:]))))
+    
+    for key in compiled:
+        print(f'{key}: {compiled[key]}\n')
                  
+
 if __name__ == '__main__':
-    print(csv_scraper('new-wordSample.csv'))
+    dict_compiler(csv_scraper('new-wordSample.csv'))
