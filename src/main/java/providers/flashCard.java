14987bcb03f8a034d9534e1ce89f221b4d0b16b1
TORITZA <terratreehive@gmail.com>
2026-07-06 08:13:34 -0400

    created flashCard & partOfSpeech, added rudimentary state to the former

---
+++ src/main/java/providers/flashCard.java
@@ -0,0 +1,15 @@
+package providers;
+
+import org.w3c.dom.Text;
+
+import java.util.LinkedHashMap;
+
+/**
+ *
+ *
+ * @author Tereza Lang (@TORITZA)
+ */
+public class flashCard {
+    private String word;
+    private LinkedHashMap<partOfSpeech, Text> definition;
+}
